049607680bcc916649533833bacd2f7af5a92625
Robin Forestier <robinforestier1407@gmail.com>
2022-10-06 14:22:21 +0200

    calcule + affichages ascii ok
    calcule mandelbrot ok
    
    création  de la surcharge

--- main.cpp
+++ main.cpp
@@ -1,19 +1,9 @@
 /*
- * for each pixel (Px, Py) on the screen do
-    x0 := scaled x coordinate of pixel (scaled to lie in the Mandelbrot X scale (-2.00, 0.47))
-    y0 := scaled y coordinate of pixel (scaled to lie in the Mandelbrot Y scale (-1.12, 1.12))
-    x := 0.0
-    y := 0.0
-    iteration := 0
-    max_iteration := 1000
-    while (x*x + y*y ≤ 2*2 AND iteration < max_iteration) do
-        xtemp := x*x - y*y + x0
-        y := 2*x*y + y0
-        x := xtemp
-        iteration := iteration + 1
-
-    color := palette[iteration]
-    plot(Px, Py, color)
+ * Labo 2 Mandelbrot set
+ *
+ * Boris Hutzli, Robin Forestier
+ * 05.10.2022 - .10.2022
+ * But:
  *
  */
 
@@ -23,21 +13,36 @@
 
 using namespace std;
 
-const int PIXELS_Y = 10;
-const int PIXELS_X = 10;
-const int MAX_ITERATIONS = 100;
+const int MAX_ITERATIONS = 1000;
 
-vector<vector<int>> mandelbrot() { // add params y0, x0
+/*
+ * mandelbrot
+ *
+ * @param
+ */
+vector<vector<int>> mandelbrot(double centreX=0, double centreY=0, double dx=3, double dy=3, int pixelSize=30)
+{
+    double x1 = centreX - (dx / 2);
+    double x2 = centreX + (dx / 2);
+    double y1 = centreY - (dy / 2);
+    double y2 = centreY + (dy / 2);
 
-    vector<vector<int>> pixelData(PIXELS_X, vector<int>(PIXELS_Y, 0));
+    mandlebrot(x1, y1, x2, y2, pixelSize);
+}
+vector<vector<int>> mandelbrot(double x1=-2, double y1=1.12, double x2=0.47, double y2=-1.12, int pixelSize=30)
+{
+    // x1;y1 coordonée en haut a gauche de notre graph
+    // x2;y2 coordonée en bas a droite de notre graph
 
-    for (int pY = 0; pY < PIXELS_Y; pY ++)
+    vector<vector<int>> pixelData(pixelSize, vector<int>(pixelSize, 0));
+
+    for (int i = 0; i < pixelSize; i++)
     {
-        for (int pX = 0; pX < PIXELS_X; pX ++)
-        {
-            double x0 = pX * 3 / (PIXELS_X / 2);
-            double y0 = pY * 2 / (PIXELS_Y / 2);
+        double x0 = x1 + ((x2 - x1) / pixelSize) * i;
 
+        for (int j = 0; j < pixelSize; j++)
+        {
+            double y0 = y1 + ((y2 -y1) / pixelSize) * j;
             double x = 0;
             double y = 0;
 
@@ -49,24 +54,17 @@ vector<vector<int>> mandelbrot() { // add params y0, x0
                 x = xtemp;
                 iterations++;
             }
-            pixelData[pX][pY] = iterations;
+            pixelData[i][j] = iterations;
         }
     }
     return pixelData;
 }
 
+
 int main() {
     vector<vector<int>> test = mandelbrot();
 
     display(test, MAX_ITERATIONS);
-
-    /*for (int i = 0; i < test.size(); i++) {
-        for (int j = 0; j < test[i].size(); j++) {
-            cout << test[i][j] << " ";
-        }
-        cout << endl;
-    }*/
-
 
     return 0;
 }
