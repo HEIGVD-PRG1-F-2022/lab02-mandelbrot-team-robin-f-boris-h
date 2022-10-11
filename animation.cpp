61b508b214025f9a1c29490b16936b3d457e8c88
borishuu <boris.hutzli@heig-vd.ch>
2022-10-11 16:32:02 +0200

    Added comments

--- animation.cpp
+++ animation.cpp
@@ -1,6 +1,11 @@
-//
-// Created by boris on 06.10.2022.
-//
+/*
+ * Fichier : animation.cpp
+ * Autheur : Boris Hutzli, Robin Forestier
+ * Date    : 13.10.22
+ *
+ * But     : Contient la fonction permettant de faire des animations sur le mandelbrot set (zoom chaque x secondes)
+ */
+
 
 #include <chrono>
 #include <thread>
@@ -11,10 +16,23 @@ using namespace std::this_thread;     // sleep_for, sleep_until
 using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
 using std::chrono::system_clock;
 
-const int MAX_ITERATIONS = 1000;
-const double ZOOM_FACTOR = 1.1;
+const int MAX_ITERATIONS = 1000;    // Nombre d'itérations
+const double ZOOM_FACTOR = 1.1;     // Le diviseur pour faire l'effet de zoom
 
-void zoom (int delay = 3, int animationCount = 10, double centreX = 0, double centreY = 0, double dx = 3, double dy = 3, int nX = 120, int nY = 60) {
+/*
+ * Permet de faire des zoom dans le set de mandelbrot
+ *
+ * @param int delay = 3 : Temps en secondes entre chaque zoom
+ * @param int animationCount = 10 : Nombre de zooms à faire
+ * @param double centreX = 0 : Coordonée X du centre.
+ * @param double centreY = 0 : Coordonée Y du centre.
+ * @param double dX = 3 : Largeur du visuel.
+ * @param double dY = 3 : Hauteur du visuel.
+ * @param int nX = 30 : Nombres de pixels horizontal.
+ * @param int nY = 30 : Nombres de pixels vertical.
+ */
+void zoom (int delay = 3, int animationCount = 10, double centreX = 0, double centreY = 0, double dx = 3,
+           double dy = 3, int nX = 60, int nY = 60) {
     for (int i = 0; i < animationCount; i++) {
         vector<vector<int>> generatedData = mandelbrot(centreX, centreY,  nX, nY, dx, dy, MAX_ITERATIONS);
         displayAscii(generatedData, MAX_ITERATIONS);
--- display.cpp
+++ display.cpp
@@ -1,21 +1,37 @@
+/*
+ * Fichier : display.cpp
+ * Autheur : Boris Hutzli, Robin Forestier
+ * Date    : 13.10.22
+ *
+ * But     : Contient des fonctions liées à l'affichage du set de mandelbrot (affichage avec des caractères ascii
+ *           ou affichage des itérations)
+ */
+
+
 #include "display.h"
 #include <vector>
 #include <iostream>
-# include <string>
 #include <iomanip> // for setw
 
 using namespace std;
 
-//const vector<int> asciiDisplays = {'.', 'o', '|', '0', '*', '#'};
-//const vector<int> asciiDisplays = {'.', ',', ';', '!', 'v', 'l', 'L', 'F', 'E', '$'};
-const vector<int> asciiDisplays = {'.', ',', '*', '/', '(', '#', '%', '&', '@'};
+//const vector<int> ASCII_DISPLAYS = {'.', 'o', '|', '0', '*', '#'};
+//const vector<int> ASCII_DISPLAYS = {'.', ',', ';', '!', 'v', 'l', 'L', 'F', 'E', '$'};
+const vector<int> ASCII_DISPLAYS = {' ', '.', ',', '*', '/', '(', '#', '%', '&', '@'};
 
+/*
+ * Affichage du set de mandelbrot en utilisant les caractères ASCII définit dans la constante ASCII_DISPLAYS
+ *
+ * @param vector<vector<int>> iterations : Vecteur contenant les nombres d'itérations pour chaque pixel
+ * @param int maxIterations : Nombre d'itérations maximum pour chaque pixel, utilisé pour calculer le char ascii à
+ *                            utiliser
+ */
 void displayAscii(vector<vector<int>> iterations, int maxIterations) {
     for (int pY = 0; pY < iterations.size(); pY ++) {
         for (int pX = 0; pX < iterations[pY].size(); pX ++) {
             int currentIterations = iterations[pX][pY];
-            int asciiIndex = (currentIterations * asciiDisplays.size() - 1) / maxIterations;
-            char currentAscii = asciiDisplays[asciiIndex];
+            int asciiIndex = (currentIterations * ASCII_DISPLAYS.size() - 1) / maxIterations;
+            char currentAscii = ASCII_DISPLAYS[asciiIndex];
             cout << currentAscii << " ";
         }
         cout << endl;
