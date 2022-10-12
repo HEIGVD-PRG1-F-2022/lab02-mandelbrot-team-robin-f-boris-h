ca8926709cc5e1decbf9b418a9dcf11f0eaa610b
Robin Forestier <robinforestier1407@gmail.com>
2022-10-12 16:33:47 +0200

    add affichage plein écran

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 
 #include <iostream>
 #include <vector>
+#include <windows.h>
 #include "mandelbrot.h"
 #include "display.h"
 #include "animation.h"
@@ -18,11 +19,23 @@ using namespace std;
 const int MAX_ITERATIONS = 1000;
 
 int main() {
-    zoom(1, 100, 0.38, -0.1);
-    //vector<vector<int>> test = mandelbrot(0.0, 0.0, 30, 30, 2.0, 2.0);
 
-    //displayAscii(test, MAX_ITERATIONS);
+    system("mode 650");
+    ShowWindow(GetConsoleWindow(), SW_MAXIMIZE);
+
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+
+    int largeur_ecran = (int)(csbi.srWindow.Right - csbi.srWindow.Left + 1);
+    int hauteur_ecran = (int)(csbi.srWindow.Bottom - csbi.srWindow.Top + 1);
+
+    //zoom(1, 100, 0.38, -0.1);
+    vector<vector<int>> test = mandelbrot(1.5, 0.0, largeur_ecran, hauteur_ecran, 6.5, 2.5);
+    // TODO: ERREUR vérifier x / y dans mandelbrot et display (invertion) !
+    displayAscii(test, MAX_ITERATIONS);
     //displayIterations(test, MAX_ITERATIONS);
+
+    cin >> hauteur_ecran;
 
     return 0;
 }
